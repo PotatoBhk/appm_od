d0bc92c449150bee43ef0785e5fd091222e154d3
Jorge Ortega <jorgeenr1995@gmail.com>
2022-02-25 14:08:34 -0500

    Progress Changes

--- app/src/main/java/com/tesisunl/appod/MainActivity.kt
+++ app/src/main/java/com/tesisunl/appod/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.tesisunl.appod
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val tvTestSocket = findViewById<TextView>(R.id.tvTestSocket)
+        val imageTest = findViewById<ImageView>(R.id.streamingView)
+
+//        SocketHandler.setSocket()
+//
+//        val mSocket = SocketHandler.getSocket()
+//
+//        mSocket.connect()
+
+//        mSocket.on("newnumber") { args ->
+//            if(args[0] != null) {
+//                println("In....")
+//                val numbers = args[0] as String
+//                runOnUiThread {
+//                    tvTestSocket.text = numbers
+//                }
+//            }
+//        }
+
+//        mSocket.on("video") { args ->
+//            if(args[0] != null) {
+//                val bytes = args[0] as ByteArray
+//                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+//                runOnUiThread {
+//                    imageTest.setImageBitmap(Bitmap.createBitmap(bmp))
+//                }
+//            }
+//        }
     }
 }
