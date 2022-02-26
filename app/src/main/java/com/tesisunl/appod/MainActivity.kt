274651b82f5a4aa3d8a34542d81995a1d7dd7d9e
Jorge Ortega <jorgeenr1995@gmail.com>
2022-02-25 21:14:24 -0500

    ViewPager for streamings

--- app/src/main/java/com/tesisunl/appod/MainActivity.kt
+++ app/src/main/java/com/tesisunl/appod/MainActivity.kt
@@ -1,17 +1,53 @@
 package com.tesisunl.appod
 
 import android.os.Bundle
+import android.widget.GridLayout
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 //        val tvTestSocket = findViewById<TextView>(R.id.tvTestSocket)
-        val imageTest = findViewById<ImageView>(R.id.streamingView)
+        val detectorImageView = findViewById<ImageView>(R.id.streamingView)
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val gridStatus = findViewById<GridLayout>(R.id.grid_activity)
+        gridStatus.columnCount = 3
+
+        detectorImageView.setImageResource(R.mipmap.test_resource_b_foreground)
+
+        val bundleA = Bundle()
+        val bundleB = Bundle()
+        bundleA.putInt("resource", R.mipmap.test_resource_a)
+        bundleB.putInt("resource", R.mipmap.test_resource_b)
+
+        val fragments: ArrayList<Fragment> = arrayListOf(
+            PageFragment(),
+            PageFragment(),
+            PageFragment()
+        )
+
+        val textView = TextView(this)
+        textView.setPadding(5,5,5,5)
+        textView.setBackgroundColor(getColor(androidx.appcompat.R.color.material_blue_grey_800))
+        textView.setTextColor(getColor(R.color.black))
+
+//        val arrayTextView : ArrayList<TextView> = ArrayList()
+
+//        for (i in 1..5) {
+//            textView.text = "Botón #$i"
+//            gridStatus.addView(textView)
+//        }
+
+        fragments[0].arguments = bundleA
+        fragments[1].arguments = bundleB
+        val adapter = ViewPagerAdapter(fragments, this)
+        viewPager.adapter = adapter
 
 //        SocketHandler.setSocket()
 //
--- app/src/main/java/com/tesisunl/appod/PageFragment.kt
+++ app/src/main/java/com/tesisunl/appod/PageFragment.kt
@@ -18,16 +18,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -40,28 +32,9 @@ class PageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        getView()?.findViewById<ImageView>(R.id.streamingView)
-            ?.setImageResource(R.drawable.test_resource_a_background)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        if(arguments != null) {
+            val resource = requireArguments().getInt("resource")
+            view.findViewById<ImageView>(R.id.streamContainer).setImageResource(resource)
+        }
     }
 }
