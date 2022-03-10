5ee9b141e7ed68ac0eae52a809c967a0072a28ce
Jorge Ortega <jorgeenr1995@gmail.com>
2022-03-09 19:32:05 -0500

    Login & Code Refactoring

--- app/src/main/java/com/tesisunl/appod/MainActivity.kt
+++ app/src/main/java/com/tesisunl/appod/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.tesisunl.appod
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
-import android.widget.GridLayout
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
@@ -16,6 +16,8 @@ import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.beust.klaxon.Klaxon
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.socket.client.Socket
 import org.json.JSONObject
 
@@ -23,22 +25,26 @@ import org.json.JSONObject
 class MainActivity : AppCompatActivity() {
     private var scale: Float = 0f
     private var widthTextView: Int = 0
+    private val listTextView = ArrayList<TextView>()
+    private var detected = false
+    private lateinit var observer: TextView
+    private lateinit var detectorImageView: ImageView
+    private lateinit var viewPager: ViewPager2
+    private lateinit var menuBar: BottomNavigationView
     private val params: LinearLayout.LayoutParams =
         LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
-    private val listTextView = ArrayList<TextView>()
-    private var detected = false
-    private lateinit var observer: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val detectorImageView = findViewById<ImageView>(R.id.streamingView)
-        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
-        val gridStatus = findViewById<GridLayout>(R.id.grid_activity)
+        detectorImageView = findViewById(R.id.streamingView)
+        viewPager = findViewById(R.id.viewPager)
+        menuBar = findViewById(R.id.bottomNavigation)
         observer = findViewById(R.id.txtDetection)
+        val gridStatus = findViewById<GridLayout>(R.id.grid_activity)
 
         detectorImageView.setImageResource(R.drawable.no_image_foreground)
 
@@ -46,14 +52,20 @@ class MainActivity : AppCompatActivity() {
 
         SocketHandler.setSocket()
         val mSocket = SocketHandler.getSocket()
-        mSocket.connect().on(Socket.EVENT_CONNECT) {
-            runOnUiThread {
-                observer.text = "NORMAL"
-                observer.setBackgroundColor(Color.parseColor("#1976D2"))
+        mSocket.connect()
+            .on(Socket.EVENT_CONNECT) {
+                runOnUiThread {
+                    observer.text = "NORMAL"
+                    observer.setBackgroundColor(Color.parseColor("#1976D2"))
+                }
+            }.on(Socket.EVENT_CONNECT_ERROR) {
+                Toast.makeText(this, "Error in websockets connection", Toast.LENGTH_LONG).show()
+            }.on(Socket.EVENT_DISCONNECT) {
+                Toast.makeText(this, "Connection with websockets has ended", Toast.LENGTH_LONG)
+                    .show()
             }
-        }
 
-        val url = "http://10.0.2.2:3000/api/get_systems"
+        val url = "http://192.168.1.9:5000/api/get_systems"
         val queue = Volley.newRequestQueue(this)
         val stringRequest = StringRequest(
             Request.Method.GET, url,
@@ -89,13 +101,32 @@ class MainActivity : AppCompatActivity() {
             })
 
         queue.add(stringRequest)
+        setListeners()
+    }
 
+    fun setListeners() {
         observer.setOnClickListener {
-            if(detected) {
+            if (detected) {
                 detected = false
                 observer.text = "NORMAL"
                 observer.setBackgroundColor(Color.parseColor("#1976D2"))
             }
+        }
+        menuBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.ic_next -> {
+                    val current = viewPager.currentItem
+                    if(current < (listTextView.size - 1))
+                        viewPager.setCurrentItem(current + 1, true)
+                }
+                R.id.ic_prev -> {
+                    val current = viewPager.currentItem
+                    if(current > 0)
+                        viewPager.setCurrentItem(current - 1, true)
+                }
+                else -> {  }
+            }
+            return@setOnItemSelectedListener true
         }
     }
 
@@ -125,6 +156,15 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
+                }
+            }
+        }
+        mSocket.on("detected") { args ->
+            if (args[0] != null) {
+                val bytes = args[0] as ByteArray
+                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                runOnUiThread {
+                    detectorImageView.setImageBitmap(Bitmap.createBitmap(bmp))
                 }
             }
         }
--- app/src/main/java/com/tesisunl/appod/SocketHandler.kt
+++ app/src/main/java/com/tesisunl/appod/SocketHandler.kt
@@ -15,7 +15,7 @@ object SocketHandler {
 // "http://localhost:3000/" will not work
 // If you want to use your physical phone you could use your ip address plus :3000
 // This will allow your Android Emulator and physical device at your home to connect to the server
-            mSocket = IO.socket("http://10.0.2.2:5000")
+            mSocket = IO.socket("http://192.168.1.9:5000")
             println("Connected...")
         } catch (e: URISyntaxException) {
             println("Doesn't WORK!!")
